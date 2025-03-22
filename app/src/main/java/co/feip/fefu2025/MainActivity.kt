47dc175f8a6d7718a0d2c25bd96ed1cd76ed9458
fsk <vovarybkin666@gmail.com>
2025-03-22 18:09:30 +1000

    hw3

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -1,26 +1,50 @@
 package co.feip.fefu2025
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import co.feip.fefu2025.databinding.MainActivityBinding
-import kotlin.random.Random
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.feip.fefu2025.ui.theme.AndroidRepoTheme
+import kotlinx.datetime.LocalDate
 
 
 class MainActivity : ComponentActivity() {
-    private val binding: MainActivityBinding by lazy {
-        MainActivityBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        binding.button.setOnClickListener {
-            val lang = LanguageLabel(this).apply {
-                name = Constants.languages.random()
-                color = -0x1000000 or Random.nextInt(0xFFFFFF)
-                percentage = Random.nextFloat() * 100
-            }
-            binding.layout.addView(lang)
+        setContent {
+            PreviewRepositoryScreen()
         }
+    }
+}
+
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+fun PreviewRepositoryScreen() {
+    AndroidRepoTheme {
+        Scaffold(
+            topBar = {
+                ButtonTopBar("android-repo")
+            }) { innerPadding ->
+            RepositoryInformation(
+                name = "android-repo",
+                description = "Repository for homework on android studio.",
+                forkNumber = 13000,
+                starNumber = 31500,
+                date = LocalDate(2025, 3, 3),
+                languages = arrayOf("C++", "Python", "Kotlin", "Java", "Lua"),
+                icon = R.drawable.ic_launcher_foreground,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            )
+        }
+
     }
 }
